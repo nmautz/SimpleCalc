ca619b4f127a5d1cf275d6ff1eb9b70b94b18c5c
nmautz <nmautz@zagmail.gonzaga.edu>
2022-02-16 17:38:19 -0800

    Initial Commit

---
+++ Shared/Simple_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_CalculatorApp.swift
+//  Shared
+//
+//  Created by Nathan Mautz on 2/16/22.
+//
+
+import SwiftUI
+
+@main
+struct Simple_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
