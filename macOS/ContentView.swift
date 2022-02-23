cf44969844a1f80aecdfb059e5a50e1a9026e519
nmautz <nmautz@zagmail.gonzaga.edu>
2022-02-23 15:35:33 -0800

    setting up mac enviroment

---
+++ macOS/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Simple Calculator (macOS)
+//
+//  Created by Nathan Mautz on 2/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
